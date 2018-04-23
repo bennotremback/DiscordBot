af823087d991f6bd3d05ca73530176e711efaa5f
Benno Tremback <benno.tremback@gmail.com>
2018-04-22 22:08:21 -0400

    add option to disable geocoding of queried location

--- plugins/weather.js
+++ plugins/weather.js
@@ -11,48 +11,69 @@ const weather = (message, args) => {
 	const geocodeKey = config.googleGeocodeKey;
 	const geocodeApi = 'https://maps.googleapis.com/maps/api/geocode/json';
 
-	snekfetch.get(geocodeApi, {
-		query: {
-			address: queryLocation,
-			key: geocodeKey,
-		},
-	}).then(geocodeResponse => {
-		if(geocodeResponse.body.status == 'ZERO_RESULTS') return;
-		const location = geocodeResponse.body.results[0].geometry.location;
-		const address = geocodeResponse.body.results[0].formatted_address;
+	if(config.geocodeWeather) {
+		snekfetch.get(geocodeApi, {
+			query: {
+				address: queryLocation,
+				key: geocodeKey,
+			},
+		}).then(geocodeResponse => {
+			if(geocodeResponse.body.status == 'ZERO_RESULTS') return;
+			const location = geocodeResponse.body.results[0].geometry.location;
+			const address = geocodeResponse.body.results[0].formatted_address;
 
+			snekfetch.get(weatherApi, {
+				query: {
+					appid: weatherKey,
+					lat: location.lat,
+					lon: location.lng,
+				},
+			}).then(weatherResponse => {
+				message.channel.send(processWeatherResponse(weatherResponse, address));
+			}).catch(console.log);
+		}).catch(console.log);
+	}
+	else {
 		snekfetch.get(weatherApi, {
 			query: {
 				appid: weatherKey,
-				lat: location.lat,
-				lon: location.lng,
+				q: queryLocation,
 			},
 		}).then(weatherResponse => {
-			let condition = weatherResponse.body.weather[0].description;
-			condition = condition.charAt(0).toUpperCase() + condition.slice(1);
-			const conditionIcon = weatherResponse.body.weather[0].icon;
-			const temperature = weatherResponse.body.main.temp;
-			const tempF = (9 / 5 * (temperature - 273) + 32).toFixed(1);
-			const tempC = (temperature - 273).toFixed(1);
-			const pressure = weatherResponse.body.main.pressure;
-			const humidity = weatherResponse.body.main.humidity;
-			const windSpeed = weatherResponse.body.wind.speed;
-			const windSpeedMiles = (windSpeed * 2.2369).toFixed(1);
-			const windSpeedKm = (windSpeed * 3.6).toFixed(1);
-
-			const weatherEmbed = new discord.RichEmbed();
-			weatherEmbed.setTitle(' ');
-			weatherEmbed.setDescription('\n');
-			weatherEmbed.setAuthor(`${condition} in ${address}`, `http://openweathermap.org/img/w/${conditionIcon}.png`);
-			weatherEmbed.setColor('0x00AE86');
-			weatherEmbed.setURL(' ');
-			weatherEmbed.addField('**Temperature**', `${tempC} °C\n${tempF} °F`, true);
-			weatherEmbed.addField('**Wind**', `${windSpeedKm} km/h\n${windSpeedMiles} mph`, true);
-			weatherEmbed.addField('**Pressure / Humidity**', `${pressure} hPa / ${humidity}%`, true);
-			message.channel.send(weatherEmbed);
+			message.channel.send(processWeatherResponse(weatherResponse));
 		}).catch(console.log);
-	}).catch(console.log);
+	}
 };
+
+const processWeatherResponse = (response, address) => {
+	let condition = response.body.weather[0].description;
+	condition = condition.charAt(0).toUpperCase() + condition.slice(1);
+	const conditionIcon = response.body.weather[0].icon;
+	const temperature = response.body.main.temp;
+	const tempF = (9 / 5 * (temperature - 273) + 32).toFixed(1);
+	const tempC = (temperature - 273).toFixed(1);
+	const pressure = response.body.main.pressure;
+	const humidity = response.body.main.humidity;
+	const windSpeed = response.body.wind.speed;
+	const windSpeedMiles = (windSpeed * 2.2369).toFixed(1);
+	const windSpeedKm = (windSpeed * 3.6).toFixed(1);
+	if(!address) {
+		address = response.body.name + ', ' + response.body.sys.country;
+	}
+
+	const weatherEmbed = new discord.RichEmbed();
+	weatherEmbed.setTitle(' ');
+	weatherEmbed.setDescription('\n');
+	weatherEmbed.setAuthor(`${condition} in ${address}`, `http://openweathermap.org/img/w/${conditionIcon}.png`);
+	weatherEmbed.setColor('0x00AE86');
+	weatherEmbed.setURL(' ');
+	weatherEmbed.addField('**Temperature**', `${tempC} °C\n${tempF} °F`, true);
+	weatherEmbed.addField('**Wind**', `${windSpeedKm} km/h\n${windSpeedMiles} mph`, true);
+	weatherEmbed.addField('**Pressure / Humidity**', `${pressure} hPa / ${humidity}%`, true);
+
+	return weatherEmbed;
+};
+
 
 module.exports = {
 	name: 'Weather',
