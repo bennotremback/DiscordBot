bf6de2e30acaef07686a5a3ae005f1ddb5e83991
trimbe <benno.tremback@gmail.com>
2018-06-19 23:37:12 -0400

    Fix for after hours detection
    Fix for after hours detection

--- plugins/stocks.js
+++ plugins/stocks.js
@@ -21,14 +21,14 @@ const stockPrice = (message) => {
 			let change = (((price - prevClose) / prevClose) * 100).toFixed(2);
 			price = price.toLocaleString('en-US', { style: 'currency', currency: currency });
 
-			if(quote.marketState == 'POST') {
+			if(quote.marketState.includes('POST')) {
 				price = quote.postMarketPrice;
 				price = price.toLocaleString('en-US', { style: 'currency', currency: currency });
 				change = quote.postMarketChangePercent.toFixed(2);
 			}
 
 			let messageResult = `${symbol} - ${name} : ${price} (${change}%)`;
-			if(quote.marketState == 'POST') messageResult += ' [After Hours]';
+			if(quote.marketState.includes('POST')) messageResult += ' [After Hours]';
 			message.channel.send(messageResult);
 		}).catch(error => {
 			// Invalid stock symbol
